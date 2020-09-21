33f18e7b455f868fabe51e4b4ccd7fa29443dd02
= <brennocesar@live.com>
2020-09-21 11:53:32 -0400

    AboutMe

--- src/Components/About/AboutComponent.js
+++ src/Components/About/AboutComponent.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './about.css'
+import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
+import {myAdventures} from '../../shared/myAdventures'
+import {Image} from 'react-bootstrap'
+
 
 const About = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === myAdventures.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? myAdventures.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    }
+    const slides = myAdventures.map((adventure) => {
+        return (
+          <CarouselItem
+            onExiting={() => setAnimating(true)}
+            onExited={() => setAnimating(false)}
+            key={adventure.src}
+          >
+            <Image className="slide-image" src={adventure.src} alt={adventure.altText} rounded/>
+            <CarouselCaption captionText={adventure.caption} captionHeader={adventure.altText} />
+          </CarouselItem>
+        );
+    })
 
     return(
         <div className="container-fluid about-container">
             <h1 className="about-title">About <s>US</s> ME!</h1>
+            <div className="row justify-content-center">
+                <div className="col-12">
+                    <h3 className="brenno-title">Hey! I am Brenno, and I am an ADVENTURER!</h3>
+                </div>
+                <div className="col-12">
+                    <h4 className="brenno-subtitle">I decided to create this application to practice my skills joining 2 things that I love. Adventures and Coding! Unfortunately I am not selling (I wish I could) those AMAZING adventurous packages</h4>
+                </div>
+                <div className="col-12">
+                    <a className="brenno-site" href="https://www.brennolima.com/">Here you can find my professional web site</a>
+                </div>
+                <div className="col-12">
+                    <h4 className="brenno-title">Check out some of my adventures! :D</h4>
+                </div>
+                <div className="col-12">
+                    <Carousel
+                        activeIndex={activeIndex}
+                        next={next}
+                        previous={previous}
+                        >
+                        <CarouselIndicators items={myAdventures} activeIndex={activeIndex} onClickHandler={goToIndex} />
+                        {slides}
+                        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+                        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+                    </Carousel>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/shared/myAdventures.js
@@ -0,0 +1,57 @@
+export const myAdventures = [
+    {
+      src: require('../assets/images/myAdventures/Brenno1.JPG'),
+      altText: 'Bluffs Park Trail',
+      caption: 'My favorite spot in Toronto!'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno2.JPG'),
+        altText: 'Bluffs Park Beach',
+        caption: 'One more from this amazing place'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno3.JPG'),
+        altText: 'I used to be a professional Go Karting Driver!',
+        caption: 'This is the picture of my last race, I got first place and won the regional championship!'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno4.JPG'),
+        altText: 'Niagara Falls',
+        caption: 'An amazing view'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno6.JPG'),
+        altText: 'Casa Loma Castle',
+        caption: 'Felt like a king!'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno7.JPG'),
+        altText: 'Bike Trail',
+        caption: 'Tommy Thompson Park'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno8.JPG'),
+        altText: 'Kayaking in Brazil!',
+        caption: 'One of my favorite things to do!'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno9.png'),
+        altText: 'Kennesaw Mountain in Atlanta',
+        caption: 'Trail/Hiking this amazing mountain in Georgia US'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno10.png'),
+        altText: 'Muskoka',
+        caption: 'Amazing Muskoka trails'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno11.png'),
+        altText: 'Bluffs Cliff',
+        caption: 'Kinda dangerous but cool and beautiful!'
+    },
+    {
+        src: require('../assets/images/myAdventures/Brenno12.png'),
+        altText: 'Stand Up Paddling',
+        caption: 'Musoka Beach Park'
+    },
+  ];
