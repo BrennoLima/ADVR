582a5604d3c9c5fca791252b04915533fe6673bc
= <brennocesar@live.com>
2020-09-23 14:49:58 -0400

    Ext

--- src/shared/myAdventures.js
+++ src/shared/myAdventures.js
@@ -1,36 +1,36 @@
 export const myAdventures = [
     {
-      src: require('../assets/images/myAdventures/Brenno1.JPG'),
+      src: require('../assets/images/myAdventures/Brenno1.jpg'),
       altText: 'Bluffs Park Trail',
       caption: 'My favorite spot in Toronto!'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno2.JPG'),
+        src: require('../assets/images/myAdventures/Brenno2.jpg'),
         altText: 'Bluffs Park Beach',
         caption: 'One more from this amazing place'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno3.JPG'),
+        src: require('../assets/images/myAdventures/Brenno3.jpg'),
         altText: 'I used to be a professional Go Karting Driver!',
         caption: 'This is the picture of my last race, I got first place and won the regional championship!'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno4.JPG'),
+        src: require('../assets/images/myAdventures/Brenno4.jpg'),
         altText: 'Niagara Falls',
         caption: 'An amazing view'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno6.JPG'),
+        src: require('../assets/images/myAdventures/Brenno6.jpg'),
         altText: 'Casa Loma Castle',
         caption: 'Felt like a king!'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno7.JPG'),
+        src: require('../assets/images/myAdventures/Brenno7.jpg'),
         altText: 'Bike Trail',
         caption: 'Tommy Thompson Park'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno8.JPG'),
+        src: require('../assets/images/myAdventures/Brenno8.jpg'),
         altText: 'Kayaking in Brazil!',
         caption: 'One of my favorite things to do!'
     },
