eccc6704b781649872a889afec47e4dbc1a04d0e
= <brennocesar@live.com>
2020-09-23 15:03:11 -0400

    Ext3

--- src/shared/myAdventures.js
+++ src/shared/myAdventures.js
@@ -1,56 +1,56 @@
 export const myAdventures = [
     {
-      src: require('../assets/images/myAdventures/Brenno1.jpg'),
+      src: require('../assets/images/myAdventures/MyAdv1.jpg'),
       altText: 'Bluffs Park Trail',
       caption: 'My favorite spot in Toronto!'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno2.jpg'),
+        src: require('../assets/images/myAdventures/MyAdv2.jpg'),
         altText: 'Bluffs Park Beach',
         caption: 'One more from this amazing place'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno3.jpg'),
+        src: require('../assets/images/myAdventures/MyAdv3.jpg'),
         altText: 'I used to be a professional Go Karting Driver!',
         caption: 'This is the picture of my last race, I got first place and won the regional championship!'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno4.jpg'),
+        src: require('../assets/images/myAdventures/MyAdv4.jpg'),
         altText: 'Niagara Falls',
         caption: 'An amazing view'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno6.jpg'),
+        src: require('../assets/images/myAdventures/MyAdv5.jpg'),
         altText: 'Casa Loma Castle',
         caption: 'Felt like a king!'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno7.jpg'),
+        src: require('../assets/images/myAdventures/MyAdv6.jpg'),
         altText: 'Bike Trail',
         caption: 'Tommy Thompson Park'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno8.jpg'),
+        src: require('../assets/images/myAdventures/MyAdv7.jpg'),
         altText: 'Kayaking in Brazil!',
         caption: 'One of my favorite things to do!'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno9.png'),
+        src: require('../assets/images/myAdventures/MyAdv8.png'),
         altText: 'Kennesaw Mountain in Atlanta',
         caption: 'Trail/Hiking this amazing mountain in Georgia US'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno10.png'),
+        src: require('../assets/images/myAdventures/MyAdv9.png'),
         altText: 'Muskoka',
         caption: 'Amazing Muskoka trails'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno11.png'),
+        src: require('../assets/images/myAdventures/MyAdv10.png'),
         altText: 'Bluffs Cliff',
         caption: 'Kinda dangerous but cool and beautiful!'
     },
     {
-        src: require('../assets/images/myAdventures/Brenno12.png'),
+        src: require('../assets/images/myAdventures/MyAdv11.png'),
         altText: 'Stand Up Paddling',
         caption: 'Musoka Beach Park'
     },
