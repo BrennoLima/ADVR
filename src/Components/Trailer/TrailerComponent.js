60f9515ea504952365f1af77987f15142dd6081a
= <brennocesar@live.com>
2020-09-28 13:50:14 -0400

    Trailer

--- src/Components/Trailer/TrailerComponent.js
+++ src/Components/Trailer/TrailerComponent.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './trailer.css'
-
+import ReactPlayer from 'react-player/lazy'
 
 const Trailer = () => {
     return(
         <div className="container-fluid trailer-container">
-            <h1 id="trailer-title" className="trailer-title">There will be an amazing trailer here soon!</h1>
+            <h1 id="trailer-title" className="trailer-title">Check out where you could be now!</h1>
+            <div className="container video-container">
+                <div className="row justify-content-center">
+                    <ReactPlayer url='https://www.youtube.com/watch?v=EYXVRBaYiaM' controls/>
+                </div>
+            </div>
+            
         </div>
     );
 }
